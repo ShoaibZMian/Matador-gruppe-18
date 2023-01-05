2d1e4b798c82c88572167d011593fe7d1527a2aa
mj23000 <markusjacobsen@hotmail.com>
2023-01-05 10:50:01 +0100

    Add the new PaymentTile

---
+++ matador/src/main/java/org/example/tiles/PaymentTile.java
@@ -0,0 +1,52 @@
+package org.example.tiles;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import org.example.Player;
+import org.example.chances.Chance;
+
+import gui_fields.GUI_Refuge;
+import gui_main.GUI;
+
+public class PaymentTile extends Tile {
+
+    private int amount;
+    private int percentage;
+    private String description;
+
+    public PaymentTile(int id, String title, String subText, String description, int amount, int percentage) {
+        this.id = id;
+        this.amount = amount;
+        this.percentage = percentage;
+        this.description = description;
+
+        this.guiField = new GUI_Refuge("", title, subText, description, Color.WHITE, Color.BLACK);
+    }
+
+    @Override
+    public boolean tileAction(Player player, Player[] players, ArrayList<Chance> chances, GUI gui) {
+        ArrayList<String> options = new ArrayList<String>();
+        options.add(Integer.toString(amount));
+
+        // Add percentage if the tile gives that option
+        String percentageString = Integer.toString(this.percentage) + "%";
+        if (this.percentage != 0) {
+            options.add(percentageString);
+        }
+
+        // Get selected option
+        String option = gui.getUserButtonPressed(player.getName() + " landede på " + this.description,
+                options.toArray(new String[options.size()]));
+
+        // Pay the payment
+        if (option == percentageString) {
+            player.setBalance((player.getBalance() * (100 - this.percentage)) / 100);
+            ;
+        } else {
+            player.setBalance(player.getBalance() - this.amount);
+        }
+
+        return true;
+    }
+}
