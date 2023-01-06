4069ac7cf5a25aaba52724f57a7cb240f3c9e4cd
shwebsen <shwebsen@gmail.com>
2023-01-06 09:00:46 +0100

    random player

--- matador/src/main/java/org/example/Main.java
+++ matador/src/main/java/org/example/Main.java
@@ -2,8 +2,13 @@ package org.example;
 
 import java.io.FileNotFoundException;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
+import com.sun.source.tree.NewArrayTree;
 import org.example.models.LanguageModel;
 
 import gui_fields.GUI_Car;
@@ -27,9 +32,16 @@ public class Main {
                                 new Player(20, 1, "Safi", GUI_Car.Type.RACECAR),
                                 new Player(20, 2, "Marc", GUI_Car.Type.TRACTOR),
                                 new Player(20, 3, "Josef", GUI_Car.Type.UFO)
+
                 };
+                List<Player> playerList = Arrays.asList(players);
+                Collections.shuffle(playerList);
+                playerList.toArray(players);
 
                 new Game(players, languageModel);
+
         }
+
+
 
 }
