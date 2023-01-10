a3a2c647e09e5f8367784846d4c93420c637eead
safi meissam <meissamkhanyahoo.com>
2023-01-10 16:55:12 +0100

    Add tests ShipTileTest færdig.

--- matador/src/test/java/org/example/PlayerTest.java
+++ matador/src/test/java/org/example/PlayerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,14 +32,13 @@ class PlayerTest {
     @DisplayName("that ship tile is functional")
     void addShipTile(){
         Player player = new Player(0,"");
-        player.addShipTile(new ShipTile(1,"",Color.RED,1001, new int[500]));
-        var tiles = player.getShipTiles();
+        player.addShipTile(new ShipTile(1,"",Color.RED,1001,new int[] { 500, 1000, 2000, 4000 }));
+        ArrayList<ShipTile> tiles = player.getShipTiles();
 
         // where tile price eq 1001
-
+        long hasObjectWithGivenPrice = tiles.stream().filter(f -> f.getPrice() == 1001).collect(Collectors.toList()).stream().count();
         // if any then test success
-        var trueval = true;
 
-        assertEquals(true, trueval);
+        assertEquals(1, hasObjectWithGivenPrice);
     }
 }
