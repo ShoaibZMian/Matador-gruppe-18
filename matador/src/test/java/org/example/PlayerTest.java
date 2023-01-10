d2e3f1860eb6a7950b9cc9129d9a8ad3e46f4058
safi meissam <meissamkhanyahoo.com>
2023-01-10 13:20:14 +0100

    Add tests

---
+++ matador/src/test/java/org/example/PlayerTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    @DisplayName("that setting the position is gettable")
+    void getSetPosition() {
+        Player player = new Player(0, "");
+        player.setPosition(20);
+        assertEquals(20, player.getPosition());
+    }
+}
