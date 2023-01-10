3d3bd2345de8a7ebc9072e04344186e360aba716
safi meissam <meissamkhanyahoo.com>
2023-01-10 15:03:45 +0100

    Add tests "outOfJail"

--- matador/src/test/java/org/example/PlayerTest.java
+++ matador/src/test/java/org/example/PlayerTest.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.chances.OutOfJailChance;
+import org.example.tiles.ShipTile;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +19,12 @@ class PlayerTest {
         assertEquals(20, player.getPosition());
     }
 
+    @Test
+    @DisplayName("that out of jail is functional")
+    void addGetOutOfJailChance() {
+        Player player = new Player(0,"");
+        player.addGetOutOfJailChance(new OutOfJailChance(""));
+        assertEquals(true,player.getOfJailChance());
+    }
 
 }
