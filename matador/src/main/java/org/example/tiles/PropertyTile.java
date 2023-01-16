13161c68f58b4485e4c2a8289ba766a264ab21cc
mj23000 <markusjacobsen@hotmail.com>
2023-01-16 10:07:34 +0100

    Clean up auction

--- matador/src/main/java/org/example/tiles/PropertyTile.java
+++ matador/src/main/java/org/example/tiles/PropertyTile.java
@@ -1,20 +1,18 @@
 package org.example.tiles;
 
-import gui_fields.GUI_Player;
 import org.apache.commons.lang3.tuple.Pair;
+import org.example.Game;
 import org.example.Player;
 
 import gui_fields.GUI_Ownable;
 import gui_fields.GUI_Street;
 import gui_main.GUI;
+import org.example.Constants;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
-
 
 public class PropertyTile extends Tile {
     protected Player owner;
@@ -108,6 +106,49 @@ public class PropertyTile extends Tile {
         setOwner(player);
     }
 
+    private void auctionAction(GUI_Ownable street, Game game) {
+        Player[] players = game.getPlayers();
+        GUI gui = game.getGui();
+        ArrayList<Pair<Player, Integer>> bids = new ArrayList<Pair<Player, Integer>>();
+
+        for (Player player : game.getPlayers()) {
+            int bid = gui.getUserInteger("Indtast dit bud " + player.getName());
+            bids.add(new Pair<Player, Integer>() {
+                @Override
+                public Player getLeft() {
+                    return players[players.length - 1];
+                }
+
+                @Override
+                public Integer getRight() {
+                    return bid;
+                }
+
+                @Override
+                public Integer setValue(Integer value) {
+                    return value;
+                }
+            });
+
+        }
+
+        // list over the players who bid, sortet by highest bid
+        List<Pair<Player, Integer>> bidsSorted = bids.stream()
+                .sorted((f1, f2) -> Integer.compare(f2.getRight(), f1.getRight()))
+                .collect(Collectors.toList());
+        // Find the highest bidder
+        int finalBid = bidsSorted.stream().findFirst().get().getRight();
+        Player finalPlayer = bidsSorted.stream().findFirst().get().getLeft();
+        // Show the name of player wich give the highest bid.
+        finalPlayer.setBalance(finalPlayer.getBalance() - finalBid);
+        street.setOwnerName(finalPlayer.getName());
+
+        setOwner(finalPlayer);
+
+        gui.showMessage(finalPlayer.getName() + " har købt grunden");
+
+    }
+
     @Override
     public void tileAction(Player player, Game game) {
 
@@ -135,63 +176,12 @@ public class PropertyTile extends Tile {
 
             switch (option) {
                 case Constants.AUCTION:
-                    // TODO Add auction
-
-// for loop igennem players
-                    int minBid = 0;
-                    int count = 0;
-
-                    ArrayList<Pair<Player,Integer>> bids = new ArrayList<Pair<Player,Integer>>();
-
-                    for (int i = 0; i < players.length ;i++) {
-                    //String option1 = gui.getUserButtonPressed("Vil du bud "+player.getName(),"BUD");
-
-                        if(!players[i].getName().equals(player.getName())){
-                            int bid = gui.getUserInteger("Indtast din bud " + players[i].getName());
-                            int finalI = i;
-                            bids.add(new Pair<Player, Integer>() {
-                                @Override
-                                public Player getLeft() {
-                                    return players[finalI];
-                                }
-
-                                @Override
-                                public Integer getRight() {
-                                    return bid;
-                                }
-
-                                @Override
-                                public Integer setValue(Integer value) {
-                                    return value;
-                                }
-                            });
-                        }
-
-                    }
-                    // list over the players who bid, sortet by highest bid
-                    List<Pair<Player,Integer>> bidsSorted = bids.stream()
-                            .sorted((f1, f2) -> Integer.compare(f2.getRight(), f1.getRight()))
-                            .collect(Collectors.toList());
-                    // Find the highest bidder
-                    int finalBid = bidsSorted.stream().findFirst().get().getRight();
-                    Player finalPlayer = bidsSorted.stream().findFirst().get().getLeft();
-                    // Show the name of player wich give the highest bid.
-                    finalPlayer.setBalance(finalPlayer.getBalance() - finalBid);
-                    street.setOwnerName(finalPlayer.getName());
-                    setOwner(finalPlayer);
-
-                    player.setBalance(player.getBalance() + finalBid);
-
-                    gui.showMessage(finalPlayer.getName() +" har købt grunden");
-
+                    auctionAction(street, game);
 
                     break;
 
                 case Constants.BUY:
                     buyAction(street, player);
-
-
-
                     break;
             }
         } else {
